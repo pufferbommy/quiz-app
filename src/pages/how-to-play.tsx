7a339c0304da866ec2bc03f221594cf4b0c56ee9
pufferbommy <pufferbommy@gmail.com>
2023-09-16 23:34:10 +0700

    feat: add two new pages are how-to-play and settings

---
+++ src/pages/how-to-play.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const HowToPlay = () => {
+  return (
+    <>
+      <h1 className="text-4xl mb-6 text-center">วิธีการเล่น</h1>
+      <div className="w-full aspect-[1/1] rounded-md bg-slate-300"></div>
+      <div className="grid grid-cols-2 gap-3 mt-6">
+        <Link href="/">
+          <Button className="w-full" variant="outline">
+            ย้อนกลับ
+          </Button>
+        </Link>
+        <Button variant="outline">ถัดไป</Button>
+      </div>
+    </>
+  );
+};
+
+export default HowToPlay;
---
+++ src/pages/settings.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
+const Settings = () => {
+  return (
+    <>
+      <h1 className="text-4xl mb-6 text-center">การตั้งค่า</h1>
+      <div className="text-center">
+        <Link href="/">
+          <Button variant="outline">ย้อนกลับ</Button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Settings;

e7f966a041016b4a949fb5f083854ec153a1cd25
pufferbommy <pufferbommy@gmail.com>
2023-09-16 23:28:10 +0700

    chore: move css and wrap app with container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../app/globals.css'
-import { AppProps } from 'next/dist/shared/lib/router/router'
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="px-3">
+      <div className="max-w-md relative mx-auto pt-36">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }

e071c424d09617a92678d82e9ec50895be505ec4
pufferbommy <pufferbommy@gmail.com>
2023-09-24 19:12:00 +0700

    feat: add logout

--- app/(user)/settings/page.tsx
+++ app/(user)/settings/page.tsx
@@ -1,16 +1,35 @@
+'use client';
+
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import Title from '@/components/text/title';
 import { Button } from '@/components/ui/button';
 import SettingsForm from '@/components/form/SettingsForm';
+import { useToast } from '@/components/ui/use-toast';
 
 const Settings = () => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    toast({
+      title: 'success',
+      description: 'ออกจากระบบสำเร็จ',
+    });
+    router.push('/auth/login');
+  };
+
   return (
     <>
       <Title>การตั้งค่า</Title>
       <SettingsForm />
+      <Button onClick={logout} variant="outline" className="w-full my-4">
+        ออกจากระบบ
+      </Button>
       <Link href="/">
-        <Button variant="outline" className="w-full mt-4">
+        <Button variant="outline" className="w-full">
           ย้อนกลับ
         </Button>
       </Link>

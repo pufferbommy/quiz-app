9338984e6d2b30f2f8cc82b2225e0a9967319800
Unknowguy <unknowguy2001@gmail.com>
2023-09-25 19:38:01 +0700

    feat: add get all for admin api

---
+++ app/api/v1/admin/verses/health/route.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+import { StatusMessageDataResponse, QuestionsData } from '@/lib/types';
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  const questions = await prisma.verse_questions.findMany({ where: { group: 'health' } });
+
+  return NextResponse.json<StatusMessageDataResponse<QuestionsData>>(
+    {
+      status: 'success',
+      message: 'Get all questions successfully',
+      data: {
+        questions: questions.map(({ id, image_path, answer }) => ({
+          id,
+          imagePath: image_path,
+          answer: JSON.parse(answer),
+        })),
+      },
+    },
+    { status: 200 }
+  );
+}

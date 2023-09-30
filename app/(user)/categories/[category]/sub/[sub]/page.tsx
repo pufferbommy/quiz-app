f6b7237375dd4be78f1dc5b22963b73d6c730846
pufferbommy <pufferbommy@gmail.com>
2023-09-30 10:56:09 +0700

    chore: fix resume game and use icon from lib

--- app/(user)/categories/[category]/sub/[sub]/page.tsx
+++ app/(user)/categories/[category]/sub/[sub]/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { Settings } from 'lucide-react';
 import { Fragment, useEffect, useState } from 'react';
 
 import Logo from '@/components/logo';
@@ -37,7 +38,7 @@ const Sub = ({ params }: { params: { category: string; sub: string } }) => {
   return (
     <>
       {isBreak ? (
-        <PauseMenu resumeGame={() => setIsBreak(true)} />
+        <PauseMenu resumeGame={() => setIsBreak(false)} />
       ) : (
         <>
           <Button
@@ -46,53 +47,14 @@ const Sub = ({ params }: { params: { category: string; sub: string } }) => {
             variant="outline"
             size="icon"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
-              <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
-            </svg>
+            <Settings className="w-4 h-4" />
           </Button>
           <h1 className="text-4xl mb-4 text-center">{title}</h1>
-          {!isLoading && questions.length > 0 && (
-            <div className="relative rounded-md border border-input overflow-hidden mb-4 aspect-video">
-              <span
-                className={`absolute z-10 inset-0 duration-300 flex justify-center items-center ${
-                  isLoadingImage ? 'opacity-100' : 'opacity-0'
-                }`}
-              >
-                กำลังโหลดรูป...
-              </span>
-              {questions.map(q => (
-                <Fragment key={q.id}>
-                  {q.id === question.id && (
-                    <Image
-                      key={q.imagePath}
-                      className={`transition-opacity duration-500 ${
-                        isLoadingImage ? 'opacity-0' : 'opacity-100'
-                      }`}
-                      sizes="100%"
-                      priority
-                      quality={75}
-                      src={q.imagePath}
-                      onLoadingComplete={() => {
-                        setIsLoadingImage(false);
-                      }}
-                      alt=""
-                      fill
-                    />
-                  )}
-                </Fragment>
-              ))}
-            </div>
-          )}
-          {!isLoading && questions.length === 0 && (
+          {isLoading ? (
+            <p className="text-center">กำลังโหลด...</p>
+          ) : questions.length === 0 ? (
             <div className="flex flex-col items-center">
-              <p>ไม่มีข้อมูล</p>
+              <p className="mb-2">ไม่พบข้อมูล</p>
               <Logo />
               <Link className="w-full" href="/">
                 <Button className="w-full" variant="outline">
@@ -100,9 +62,38 @@ const Sub = ({ params }: { params: { category: string; sub: string } }) => {
                 </Button>
               </Link>
             </div>
-          )}
-          {!isLoading && questions.length > 0 && (
+          ) : (
             <>
+              <div className="relative rounded-md border border-input overflow-hidden mb-4 aspect-video">
+                <span
+                  className={`absolute z-10 inset-0 duration-300 flex justify-center items-center ${
+                    isLoadingImage ? 'opacity-100' : 'opacity-0'
+                  }`}
+                >
+                  กำลังโหลดรูป...
+                </span>
+                {questions.map(q => (
+                  <Fragment key={q.id}>
+                    {q.id === question.id && (
+                      <Image
+                        key={q.imagePath}
+                        className={`transition-opacity duration-500 ${
+                          isLoadingImage ? 'opacity-0' : 'opacity-100'
+                        }`}
+                        sizes="100%"
+                        priority
+                        quality={75}
+                        src={q.imagePath}
+                        onLoadingComplete={() => {
+                          setIsLoadingImage(false);
+                        }}
+                        alt=""
+                        fill
+                      />
+                    )}
+                  </Fragment>
+                ))}
+              </div>
               {category === 'verse' ? (
                 <VerseForm
                   isLoadingImage={isLoadingImage}

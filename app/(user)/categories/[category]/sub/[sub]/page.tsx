788a184e37dc5bbdf5d311a1e0b016c837d7ae67
pufferbommy <pufferbommy@gmail.com>
2023-09-30 11:06:54 +0700

    refactor: improve naming very nice

--- app/(user)/categories/[category]/sub/[sub]/page.tsx
+++ app/(user)/categories/[category]/sub/[sub]/page.tsx
@@ -16,19 +16,20 @@ const Sub = ({ params }: { params: { category: string; sub: string } }) => {
   const { category, sub } = params;
   const url = `/api/v1/jokes/${category}s/${sub}`;
   const { questions, isLoading, shuffleQuestions } = useQuestions(url);
-  const [questionIndex, setQuestionIndex] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [isBreak, setIsBreak] = useState(false);
   const [isLoadingImage, setIsLoadingImage] = useState(true);
 
   const title = category === 'verse' ? 'กลอนปริศนา' : 'โจ๊กภาพปริศนา';
-  const question = questions[questionIndex] || null;
+  const question = questions[currentQuestionIndex] || null;
 
   const nextQuestion = () => {
-    if (questionIndex === questions.length - 1) {
+    const lastQuestionIndex = questions.length - 1;
+    if (currentQuestionIndex === lastQuestionIndex) {
       shuffleQuestions();
-      setQuestionIndex(0);
+      setCurrentQuestionIndex(0);
     } else {
-      setQuestionIndex(prev => prev + 1);
+      setCurrentQuestionIndex(prev => prev + 1);
     }
   };
 
